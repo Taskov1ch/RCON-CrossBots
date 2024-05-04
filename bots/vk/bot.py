2eb0ead2c384d2f10b75db189f2206590a6fadf5
Tynaev Taskovich <154591704+Taskov1ch@users.noreply.github.com>
2024-05-05 04:34:21 +0600

    Translate

--- bots/vk/bot.py
+++ bots/vk/bot.py
@@ -2,8 +2,8 @@ import argparse
 import os
 import sys
 
-parser = argparse.ArgumentParser(description = "Для получения рабочего каталога")
-parser.add_argument("main_path", type = str, help = "Рабочий каталог")
+parser = argparse.ArgumentParser(description = "To get the working directory")
+parser.add_argument("main_path", type = str, help = "Working directory")
 args = parser.parse_args()
 sys.path.append(args.main_path)
 os.chdir(args.main_path)
